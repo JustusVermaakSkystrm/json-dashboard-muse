4496a5faf3f801e130db33b289ad727dbcce46cd
JustusVermaakSkystrm <152275518+JustusVermaakSkystrm@users.noreply.github.com>
2025-02-13 12:34:30 +0000

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id"
+  apiKey: "AIzaSyD0iZk_x9g2vfSI-yjdcSJ9Mfq9G5Z0eTc",
+  authDomain: "loveablefeb25.firebaseapp.com",
+  projectId: "loveablefeb25",
+  storageBucket: "loveablefeb25.firebasestorage.app",
+  messagingSenderId: "896531660566",
+  appId: "1:896531660566:web:9175732c4fde506ca11a37",
+  measurementId: "G-P5WCGP4V1F"
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
